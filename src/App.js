f3fcc31d275573e2f1fc8c645d85e2441fc29483
Swastik Bose <98341839+swastik-bose007@users.noreply.github.com>
2023-03-10 00:58:47 +0530

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import logo from './logo.svg';
+// ------------------------------------------------------
+// Prerequisites
+// ------------------------------------------------------
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+// ------------------------------------------------------
+// Route Navigations
+// ------------------------------------------------------
+import RootLayout from "./App/Routes/RootLayout";
+
+// ------------------------------------------------------
+// UI Components
+// ------------------------------------------------------
+import Home from "./App/Component/Home";
+import About from "./App/Component/About";
+import Skills from "./App/Component/Skills";
+import Projects from "./App/Component/Projects";
+import Experience from "./App/Component/Experience";
+import Contact from "./App/Component/Contact";
+// ------------------------------------------------------
+// UI Style sheets
+// ------------------------------------------------------
 import './App.css';
 
+
+// ------------------------------------------------------
+// Web Routes
+// ------------------------------------------------------
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <RootLayout />,
+    children : [
+      {
+        path : "/",
+        element : <Home />
+      },
+      {
+        path : "/about",
+        element : <About />
+      },
+      {
+        path : "/skills",
+        element : <Skills />
+      },
+      {
+        path : "/projects",
+        element : <Projects />
+      },
+      {
+        path : "/experience",
+        element : <Experience />
+      },
+      {
+        path : "/contact",
+        element : <Contact />
+      },
+    ],
+  },
+]);
+
+// ------------------------------------------------------
+// UI Rendering
+// ------------------------------------------------------
 function App() {
   return (
     <>
-      <h1>kkkkkkkkkkkkkkk</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
